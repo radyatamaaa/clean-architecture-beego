6643eb009272e312abb7f8ea8a7b5e480cee6d86
ajitimurkp <aji.muhammad@kpvendor.id>
2022-02-04 13:26:18 +0700

    complete customer_service test

--- internal/customer/delivery/grpc/customer_service_test.go
+++ internal/customer/delivery/grpc/customer_service_test.go
@@ -36,3 +36,110 @@ func TestCustomerService_GetCustomers(t *testing.T) {
 	}
 
 }
+
+func TestCustomerService_GetCustomerById(t *testing.T) {
+	for i := 0; i < 1; i++ {
+		t.Run("TestCustomerService_GetCustomerById-success", func(t *testing.T) {
+			t.Parallel()
+			var connection *grpc.ClientConn
+			connection, err := grpc.Dial("localhost:9090", grpc.WithInsecure())
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+			defer connection.Close()
+			req := &GetCustomerByIdParams{
+				state:         protoimpl.MessageState{},
+				sizeCache:     0,
+				unknownFields: nil,
+				Id:            1,
+			}
+			client := NewCustomerServiceClient(connection)
+			resp, err := client.GetCustomerById(context.Background(), req)
+			t.Log("Response: ", resp)
+			log.Printf("Response: %+v", resp)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestCustomerService_StoreCustomer(t *testing.T) {
+	for i := 0; i < 1; i++ {
+		t.Run("TestCustomerService_StoreCustomer-success", func(t *testing.T) {
+			t.Parallel()
+			var connection *grpc.ClientConn
+			connection, err := grpc.Dial("localhost:9090", grpc.WithInsecure())
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+			defer connection.Close()
+			req := &CustomerStoreRequest{
+				state:         protoimpl.MessageState{},
+				sizeCache:     0,
+				unknownFields: nil,
+				CustomerName:  "Test Name",
+				Phone:         "0811223344556",
+				Email:         "test@mail.com",
+				Address:       "Jl. test, Jakarta",
+			}
+			client := NewCustomerServiceClient(connection)
+			resp, err := client.StoreCustomer(context.Background(), req)
+			t.Log("Response: ", resp)
+			log.Printf("Response: %+v", resp)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestCustomerService_UpdateCustomer(t *testing.T) {
+	for i := 0; i < 1; i++ {
+		t.Run("TestCustomerService_UpdateCustomer-success", func(t *testing.T) {
+			t.Parallel()
+			var connection *grpc.ClientConn
+			connection, err := grpc.Dial("localhost:9090", grpc.WithInsecure())
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+			defer connection.Close()
+			req := &CustomerUpdateRequest{
+				state:         protoimpl.MessageState{},
+				sizeCache:     0,
+				unknownFields: nil,
+				Id:            1,
+				CustomerName:  "Test Update",
+				Phone:         "0811223344556",
+				Email:         "testupdate@mail.com",
+				Address:       "Jl. testupdate, Jakarta",
+			}
+			client := NewCustomerServiceClient(connection)
+			resp, err := client.UpdateCustomer(context.Background(), req)
+			t.Log("Response: ", resp)
+			log.Printf("Response: %+v", resp)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestCustomerService_DeleteCustomer(t *testing.T) {
+	for i := 0; i < 1; i++ {
+		t.Run("TestCustomerService_DeleteCustomer-success", func(t *testing.T) {
+			t.Parallel()
+			var connection *grpc.ClientConn
+			connection, err := grpc.Dial("localhost:9090", grpc.WithInsecure())
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+			defer connection.Close()
+			req := &GetCustomerByIdParams{
+				state:         protoimpl.MessageState{},
+				sizeCache:     0,
+				unknownFields: nil,
+				Id:            1,
+			}
+			client := NewCustomerServiceClient(connection)
+			resp, err := client.DeleteCustomer(context.Background(), req)
+			t.Log("Response: ", resp)
+			log.Printf("Response: %+v", resp)
+			assert.NoError(t, err)
+		})
+	}
+}

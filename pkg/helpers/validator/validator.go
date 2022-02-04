aed959f4e4708c16e6a5883efe6bb1336e4e6404
radyatamaa <97084229+radyatamaaa@users.noreply.github.com>
2022-02-04 16:15:01 +0700

    Update validator.go

--- pkg/helpers/validator/validator.go
+++ pkg/helpers/validator/validator.go
@@ -2,10 +2,9 @@ package validator
 
 import (
 	"clean-architecture-beego/pkg/helpers/converter_value"
-	"database/sql/driver"
-	"encoding/json"
 	"fmt"
 	"github.com/go-playground/validator/v10"
+	"github.com/prometheus/common/log"
 	"reflect"
 	"strings"
 )
@@ -20,9 +19,9 @@ func (v *GlobalValidator) Validate(i interface{}) error {
 }
 
 func (v *GlobalValidator) RegisterValidators() error {
-	//if err := v.Validator.RegisterValidation("customer_photo_document", CustomerPhotoDocument); err != nil {
-	//	log.Error(err)
-	//}
+	if err := v.Validator.RegisterValidation("validate_required_if_another_filed", validateRequireIfAnotherField); err != nil {
+		log.Error(err)
+	}
 
 	return nil
 }
@@ -108,53 +107,5 @@ func PanicIf(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
-}
-func GetValueAndColumnStructToDriverValue(value interface{}) ([]driver.Value, []string) {
-	var result []driver.Value
-
-	//column
-	j, _ := json.Marshal(value)
-	// a map container to decode the JSON structure into
-	c := make(map[string]json.RawMessage)
-
-	// unmarschal JSON
-	e := json.Unmarshal(j, &c)
-
-	// panic on error
-	if e != nil {
-		panic(e)
-	}
-
-	// a string slice to hold the keys
-	k := make([]string, len(c))
-
-	// iteration counter
-	i := 0
-
-	// copy c's keys into k
-	for s, e := range c {
-		k[i] = s
-		v, _ := e.MarshalJSON()
-		var val driver.Value
-		err := json.Unmarshal(v, &val)
-		if err != nil {
-			panic(err)
-		}
-
-		//dv := driver.Value(v.Interface())
-		if reflect.TypeOf(val).String() == "string" {
-			valueString := reflect.ValueOf(val).String()
-			convertDate := converter_value.StringToDateTimeNullable(valueString)
-			if convertDate.IsZero() == false {
-				val = convertDate
-			}
-		}
-
-		result = append(result, val)
-		i++
-	}
-
-	return result, k
-
 }
 

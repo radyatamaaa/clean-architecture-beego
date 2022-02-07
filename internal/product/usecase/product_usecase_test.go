b8cbf4f64357829cfc07cad87fa9cebd0daae7aa
radyatamaa <97084229+radyatamaaa@users.noreply.github.com>
2022-02-08 02:29:43 +0700

    ADD test

--- internal/product/usecase/product_usecase_test.go
+++ internal/product/usecase/product_usecase_test.go
@@ -17,22 +17,23 @@ var (
 )
 func TestProductUseCase_GetProducts(t *testing.T) {
 	//resultMock
-	mockProduct := domain.Product{}
+	mockProduct := []domain.Product{}
 	err := faker.FakeData(&mockProduct)
 	assert.NoError(t, err)
 
-	id := mockProduct.Id
+	limit := 5
+	offset := 0
 
 	t.Run("success", func(t *testing.T) {
 		//repositoryMock
 		mockProductRepository := new(_mockProductRepository.Repository)
 
-		mockProductRepository.On("FindByID", mock.Anything,
-			mock.AnythingOfType("uint")).Return(mockProduct, nil).Once()
+		mockProductRepository.On("Fetch", mock.Anything,
+			mock.AnythingOfType("int"),mock.AnythingOfType("int")).Return(mockProduct, nil).Once()
 
 		u := usecase.NewProductUseCase(timeoutContext,mockProductRepository)
 
-		a, err := u.GetProductById(context.TODO(), id)
+		a, err := u.GetProducts(context.TODO(), limit,offset)
 
 		assert.NoError(t, err)
 		assert.NotNil(t, a)
@@ -40,16 +41,16 @@ func TestProductUseCase_GetProducts(t *testing.T) {
 		mockProductRepository.AssertExpectations(t)
 	})
 
-	t.Run("error-FindByID-function", func(t *testing.T) {
+	t.Run("error-Fetch-function", func(t *testing.T) {
 		//repositoryMock
 		mockProductRepository := new(_mockProductRepository.Repository)
 
-		mockProductRepository.On("FindByID", mock.Anything,
-			mock.AnythingOfType("uint")).Return(domain.Product{}, errors.New("invalid query")).Once()
+		mockProductRepository.On("Fetch", mock.Anything,
+			mock.AnythingOfType("int"),mock.AnythingOfType("int")).Return([]domain.Product{}, errors.New("invalid query")).Once()
 
 		u := usecase.NewProductUseCase(timeoutContext,mockProductRepository)
 
-		_, err := u.GetProductById(context.TODO(), id)
+		_, err := u.GetProducts(context.TODO(), limit,offset)
 
 		assert.Error(t, err)
 
@@ -100,40 +101,37 @@ func TestProductUseCase_GetProductById(t *testing.T) {
 }
 
 func TestProductUseCase_SaveProduct(t *testing.T) {
-	//resultMock
-	mockProduct := domain.Product{}
+	//requestMock
+	mockProduct := domain.ProductStoreRequest{}
 	err := faker.FakeData(&mockProduct)
 	assert.NoError(t, err)
-
-	id := mockProduct.Id
 
 	t.Run("success", func(t *testing.T) {
 		//repositoryMock
 		mockProductRepository := new(_mockProductRepository.Repository)
 
-		mockProductRepository.On("FindByID", mock.Anything,
-			mock.AnythingOfType("uint")).Return(mockProduct, nil).Once()
+		mockProductRepository.On("Store", mock.Anything,
+			mock.AnythingOfType("domain.Product")).Return(nil).Once()
 
 		u := usecase.NewProductUseCase(timeoutContext,mockProductRepository)
 
-		a, err := u.GetProductById(context.TODO(), id)
+		err := u.SaveProduct(context.TODO(), mockProduct)
 
 		assert.NoError(t, err)
-		assert.NotNil(t, a)
 
 		mockProductRepository.AssertExpectations(t)
 	})
 
-	t.Run("error-FindByID-function", func(t *testing.T) {
+	t.Run("error-Store-function", func(t *testing.T) {
 		//repositoryMock
 		mockProductRepository := new(_mockProductRepository.Repository)
 
-		mockProductRepository.On("FindByID", mock.Anything,
-			mock.AnythingOfType("uint")).Return(domain.Product{}, errors.New("invalid query")).Once()
+		mockProductRepository.On("Store", mock.Anything,
+			mock.AnythingOfType("domain.Product")).Return(errors.New("invalid query")).Once()
 
 		u := usecase.NewProductUseCase(timeoutContext,mockProductRepository)
 
-		_, err := u.GetProductById(context.TODO(), id)
+		err := u.SaveProduct(context.TODO(), mockProduct)
 
 		assert.Error(t, err)
 
@@ -143,39 +141,37 @@ func TestProductUseCase_SaveProduct(t *testing.T) {
 
 func TestProductUseCase_UpdateProduct(t *testing.T) {
 	//resultMock
-	mockProduct := domain.Product{}
+	mockProduct := domain.ProductUpdateRequest{}
 	err := faker.FakeData(&mockProduct)
 	assert.NoError(t, err)
 
-	id := mockProduct.Id
 
 	t.Run("success", func(t *testing.T) {
 		//repositoryMock
 		mockProductRepository := new(_mockProductRepository.Repository)
 
-		mockProductRepository.On("FindByID", mock.Anything,
-			mock.AnythingOfType("uint")).Return(mockProduct, nil).Once()
+		mockProductRepository.On("Update", mock.Anything,
+			mock.AnythingOfType("domain.Product")).Return(nil).Once()
 
 		u := usecase.NewProductUseCase(timeoutContext,mockProductRepository)
 
-		a, err := u.GetProductById(context.TODO(), id)
+		err := u.UpdateProduct(context.TODO(), mockProduct)
 
 		assert.NoError(t, err)
-		assert.NotNil(t, a)
 
 		mockProductRepository.AssertExpectations(t)
 	})
 
-	t.Run("error-FindByID-function", func(t *testing.T) {
+	t.Run("error-Store-function", func(t *testing.T) {
 		//repositoryMock
 		mockProductRepository := new(_mockProductRepository.Repository)
 
-		mockProductRepository.On("FindByID", mock.Anything,
-			mock.AnythingOfType("uint")).Return(domain.Product{}, errors.New("invalid query")).Once()
+		mockProductRepository.On("Update", mock.Anything,
+			mock.AnythingOfType("domain.Product")).Return(errors.New("invalid query")).Once()
 
 		u := usecase.NewProductUseCase(timeoutContext,mockProductRepository)
 
-		_, err := u.GetProductById(context.TODO(), id)
+		err := u.UpdateProduct(context.TODO(), mockProduct)
 
 		assert.Error(t, err)
 
